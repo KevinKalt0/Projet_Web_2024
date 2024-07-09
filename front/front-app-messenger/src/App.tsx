3c8a55820ba798b420cb5f013956e55d1cb52e10
1dylanleray <dleray77@outlook.fr>
2024-07-09 17:53:16 +0200

    feat(pw): init frontend

---
+++ front/front-app-messenger/src/App.tsx
@@ -0,0 +1,14 @@
+
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <h1 className='text-blue-500'>WAGWAN FAM !</h1>
+  )
+}
+
+export default App
